d5a457dc932760d95b397c6b8de2abfb1753b1e9
Louis Toro <louistororosario@gmail.com>
2023-10-12 13:50:41 -0400

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char character
+	int integer
+	long int long_int
+	long long int doble_long_int
+	float floating
+
+	printf("Size of char: %zu byte(s)\n"(character));
+	printf("Size of int: %zu byte(s)\n"(integer));
+	printf("Size of long int: %zu byte(s)\n"(long_int));
+	printf("Size of long long int: %zu byte(s)\n"(doble_long_int));
+	printf("Size of float: %zu byte(s)\n"(floating));
+	return (0);
+}
