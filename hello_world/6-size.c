3865aa39b9a2f0d519bb31a64d0ed68845154cb8
Louis Toro <louistororosario@gmail.com>
2023-10-12 14:14:21 -0400

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-	char character
-	int integer
-	long int long_int
-	long long int doble_long_int
-	float floating
+	char character;
+	int integer;
+	long int long_int;
+	long long int doble_long_int;
+	float floating;
 
-	printf("Size of char: %zu byte(s)\n", sizeof(character));
-	printf("Size of int: %zu byte(s)\n", sizeof(integer));
-	printf("Size of long int: %zu byte(s)\n", sizeof(long_int));
-	printf("Size of long long int: %zu byte(s)\n", sizeof(doble_long_int));
-	printf("Size of float: %zu byte(s)\n", sizeof(floating));
+	printf("Size of a char: %zu byte(s)\n", sizeof(character));
+	printf("Size of an int: %zu byte(s)\n", sizeof(integer));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long_int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(doble_long_int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floating));
 	return (0);
 }
