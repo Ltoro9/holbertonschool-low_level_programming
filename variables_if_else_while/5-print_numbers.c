bd1e6d2ba3774d26853d6a0195da478241c0d5f3
Louis Toro <louistororosario@gmail.com>
2023-10-18 21:24:49 -0400

    numeros

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main- funcion del main entry
+ *
+ *
+ *Return: devolver 0
+ *
+*/
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
