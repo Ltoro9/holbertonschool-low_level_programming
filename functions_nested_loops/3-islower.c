960902ef650adb0e365c01f94b5cba925a8efd1c
Louis Toro <louistororosario@gmail.com>
2023-10-19 11:50:20 -0400

    lowerc

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_islower - Tells you if c is lower case
+ *
+ *@c: stores return from is lower , 1 for lower
+ *Return: c value
+ */
+int _islower(int c)
+{
+
+	if (islower(c))
+	{
+		c = 1;
+	}
+	else
+	{
+		c = 0;
+	}
+return (c);
+}
