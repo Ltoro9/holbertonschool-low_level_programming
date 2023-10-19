449a36b47e7c0300935c7cb41081f7ad0f597ab8
Louis Toro <louistororosario@gmail.com>
2023-10-19 13:08:23 -0400

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *_abs - Prints absolute values
+ *
+ *@n: stores value from absolut number
+ *Return: n value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+	n = (n * (-1));
+	}
+
+
+return (n);
+}
