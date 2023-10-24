ef109daed040814650755e1209f5702412af4931
Louis Toro <louistororosario@gmail.com>
2023-10-24 11:08:34 -0400

    copy a string

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ **_strcpy - copies the string pointed to by src
+ *
+ *Return: the pointer to dest
+ *
+ *@dest: first variable
+ *
+ *@src: second variable
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, l;
+
+	l = 0;
+	for (i = 0; i < l && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < l; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return dest;
+}
