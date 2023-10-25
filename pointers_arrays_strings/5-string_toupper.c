fc4bbc5e5022380b8a2892fee4705b2e71e0a094
Louis Toro <louistororosario@gmail.com>
2023-10-24 22:41:47 -0400

    string to upper

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,26 +1,21 @@
 #include "main.h"
 
 /**
- * string_toupper - Convert lowercase
- * characters to uppercase in a string.
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
+ * @s: string to modify
  *
- * @str: The input string to be modified.
- *
- * Return: A pointer to the modified
- * string.
+ * Return: the resulting string
  */
-
-char *string_toupper(char *str)
+char *string_toupper(char *s)
 {
-	char *copy = str;
+	int i;
 
-	while (*copy != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (*copy >= 'a' && *copy <= 'z')
-		{
-			*copy = *copy - ('a' - 'A');
-		}
-		copy++;
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] + 32;
 	}
-	return (str);
+
+	return (s);
 }
