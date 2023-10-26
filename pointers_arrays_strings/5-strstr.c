7b2062455e3db842ff13d57774336b393157809e
Louis Toro <louistororosario@gmail.com>
2023-10-26 00:10:54 -0400

    locates a substring

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *_strstr - locates a substring.
+ *@haystack: string to search
+ *@needle: substring to look for
+ *Return: NULL if the substring is not found
+ *
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *ltNull = '\0';
+
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] != needle[j])
+			{
+				return (needle);
+			}
+			if (!needle[j])
+			{
+				return (&haystack[i]);
+			}
+		}
+	}
+	return (ltNull);
+}
