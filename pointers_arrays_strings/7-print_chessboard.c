6a376ae4b1030df954122119a4d9aa07f4edda5c
Louis Toro <louistororosario@gmail.com>
2023-10-26 10:26:34 -0400

    prints the chessboard.

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_chessboard - prints the chessboard.
+ *@a: 2d array 
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, l, pieza;
+
+	for (i = 0; i < 8; i++)
+	{
+
+		for (l = 0; l < 8; l++)
+		{
+			pieza = (a[i][l]);
+			_putchar(pieza);
+		}
+	_putchar('\n');
+	}
+}
