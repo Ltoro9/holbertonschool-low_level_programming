c50f06deacebdd894d128450b6c865b47e54a26b
Louis Toro <louistororosario@gmail.com>
2023-10-26 13:14:29 -0400

    substring

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -14,11 +14,11 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j += 2)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (haystack[i] != needle[j])
 			{
-				return (needle);
+				return (needle++);
 			}
 			if (!needle[j])
 			{
