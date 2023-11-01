9e45a589756af9177cfefe872fe0007e086ad86d
Louis Toro <louistororosario@gmail.com>
2023-11-01 11:06:57 -0400

    what is my name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- *
- *
- *
+ *main - program that prints its name, followed by a new line.
+ *@argc: variable
+ *@argv: value of string
+ *Return: value 0
  */
 int main(int argc, char *argv[])
 {
