ca8536c2350d9c737883d034043b4b28f7d6cc64
Louis Toro <louistororosario@gmail.com>
2023-11-06 20:24:52 -0400

    concatenates two strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,6 +12,10 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *result;
+	unsigned int len1;
+	unsigned int len2;
+
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -20,15 +25,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	}
 
-	unsigned int len1 = strlen(s1);
-	unsigned int len2 = strlen(s2);
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 
 	if (n >= len2)
 	{
 		n = len2;
 	}
 
-	char *result = malloc(len1 + n + 1);
+	result = malloc(len1 + n + 1);
 
 	if (result == NULL)
 	{
