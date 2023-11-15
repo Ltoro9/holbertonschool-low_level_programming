58983ce18524c2eb910cb379a03b716dae1dce29
Louis Toro <louistororosario@gmail.com>
2023-11-15 10:54:44 -0400

    the sum of all

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,20 +12,18 @@ int sum_them_all(const unsigned int n, ...)
 	int result;
 
 	va_list args;
-	va_start (args, n);
+	va_start(args, n);
 
 	result = 0;
 
-	if (n == 0){
+	if (n == 0)
 		return (0);
-	}
 
-	for (i = 0; i < n; i++){
+	for (i = 0; i < n; i++)
 		result += va_arg(args, int);
-	}
 
 	va_end(args);
 
-	return result;
+	return (result);
 
 }
