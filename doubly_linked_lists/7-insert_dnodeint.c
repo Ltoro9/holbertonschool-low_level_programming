4797ed81d51127caeb911fb5dcd9ced6b610c444
Louis Toro <louistororosario@gmail.com>
2023-11-30 15:03:37 -0400

    function that inserts a new node at a given position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,45 +10,42 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node;
-	dlistint_t *temp;
+	dlistint_t *new_node, *current;
 	unsigned int count = 0;
 
-	new_node = malloc(sizeof(dlistint_t));
-
-	if (new_node == NULL)
+	if (h == NULL)
 		return (NULL);
 
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
-
-
-	if (*h == NULL && idx == 0)
-	{
-		*h = new_node;
-		return (new_node);
-	}
-	temp = *h;
-	while (temp != NULL && count < idx - 1)
-	{
-		temp = temp->next;
-		count++;
-	}
 	if (idx == 0)
 	{
-		new_node->next = *h;
-		(*h)->prev = new_node;
+		if (*h != NULL)
+		{
+			new_node->next = *h;
+			(*h)->prev = new_node;
+		}
 		*h = new_node;
-	}
-	if (count != idx)
-	{
-		new_node->next = temp->next;
-		if (temp->next != NULL)
-			new_node->prev = temp;
-		temp->next = new_node;
 		return (new_node);
 	}
-	free(new_node);
-	return (NULL);
+	current = *h;
+	while (current != NULL)
+	{
+		if (count == idx)
+		{
+			new_node->next = current;
+			new_node->prev = current->prev;
+			current->prev->next = new_node;
+			current->prev = new_node;
+			return (new_node);
+		}
+		current = current->next;
+		count++;
+	}
+	new_node = NULL;
+	if (count == idx)
+		new_node = add_dnodeint_end(h, n);
+	return (new_node);
 }
