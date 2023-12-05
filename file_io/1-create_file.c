90ff45478892c7276fd212d1a70687554944ccbb
Louis Toro <louistororosario@gmail.com>
2023-12-04 21:22:53 -0400

    function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -5,33 +5,32 @@
  * create_file - function that create file
  * @filename: name of file to create
  * @text_content:  is a NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure (file can not be created,
- * file can not be written, write “fails”,
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-    int archivo, rw;
+	int archivo, rw;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    archivo = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	archivo = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-    if (archivo == -1)
-        return (-1);
+	if (archivo == -1)
+		return (-1);
 
-    if (text_content == NULL)
-        text_content = "";
+	if (text_content == NULL)
+		text_content = "";
 
-    rw = (int)write(archivo, text_content, strlen(text_content));
+	rw = (int)write(archivo, text_content, strlen(text_content));
 
-    if (rw == -1)
-    {
-        close(archivo);
-        return (-1);
-    }
+	if (rw == -1)
+	{
+		close(archivo);
+		return (-1);
+	}
 
-    close(archivo);
+	close(archivo);
 
-    return (1);
+	return (1);
 }
